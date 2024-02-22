c37e572c5aede1ec475739579abc8e91b2d32fb7
Hibari1020 <hirokiyokfujita@icloud.com>
2024-02-22 22:31:25 +0900

    Make micropost model and controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
 # メインのサンプルユーザーを1人作成する
-User.create!(name:  "Hibari",
-    email: "hibari@railstutorial.org",
-    password:              "1010yoko",
-    password_confirmation: "1010yoko",
-    admin: true)
+user1 = User.create!(name:  "Hibari",
+                     email: "hibari@railstutorial.org",
+                     password:              "1010yoko",
+                     password_confirmation: "1010yoko",
+                     admin: true)
+
+user1.microposts.create!(content: "ランニングをする")
+user1.microposts.create!(content: "英語の勉強をする")
 
 # サンプルユーザー
 User.create!(name:  "Makiko",
